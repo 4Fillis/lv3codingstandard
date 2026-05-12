542bf201bf5343c181ef238c4169d7aca0f2ad02
4Fillis <penguinstakingover@gmail.com>
2026-05-13 10:16:30 +1200

    Colors for interface added

--- file.py
+++ file.py
@@ -1,1 +1,14 @@
-#SQL path storage method
+#importing libraries
+import tkinter as tk
+from functools import partial
+import random
+
+#main colors for gui
+txt_clr = "#0e021b" #main text highlight color
+txtbg_bar_clr = "#e3d5f1" #input bar bg
+txt_bar_clr = "#390846" #user input text color
+bg_clr = "#C0B738" #main text color
+btn_press_bg = "#C0B738"
+btn_txt_clr = "#2E2E2B"
+font = "OpenDyslexic"
+fontsize = 12
