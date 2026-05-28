8ce9fbc6b55d89bcb390603f11a282bcc2d4e338
4Fillis <penguinstakingover@gmail.com>
2026-05-28 15:09:40 +1200

    player fully variabl-ised for code futureproofing and conveinience

--- gamefile.py
+++ gamefile.py
@@ -6,6 +6,15 @@ import time
 #color variables
 bg_clr = (251, 247, 215)
 plyr_clr = (254, 200, 216)
+
+#main sprite variables
+plyr_speed = 5
+xpos = 100
+ypos = 100
+hght = 20
+wdth = 20
+
+
 #starting pygame
 pygame.init()
 #creating window
@@ -15,13 +24,9 @@ screen.fill(bg_clr)
 #clock for making game run at 60fps to avoid crashes
 clock = pygame.time.Clock()
 
-#main sprite
-plyr_speed = 5
-x = 100
-y = 100
+
 
 rungame = True
-
 
 while rungame == True:
     #if the user quits the window
@@ -32,17 +37,16 @@ while rungame == True:
     
     #Checking for move key inputs
     press = pygame.key.get_pressed()
-    if press[pygame.K_UP]: y-=plyr_speed
-    if press[pygame.K_DOWN]: y+=plyr_speed
-    if press[pygame.K_LEFT]: x-=plyr_speed
-    if press[pygame.K_RIGHT]: x+=plyr_speed
+    if press[pygame.K_UP]: ypos-=plyr_speed
+    if press[pygame.K_DOWN]: ypos+=plyr_speed
+    if press[pygame.K_LEFT]: xpos-=plyr_speed
+    if press[pygame.K_RIGHT]: xpos+=plyr_speed
 
 
-    # 4. Redraw Everything
+    #drawing sprite
     screen.fill(bg_clr) # Clear screen with black
-    pygame.draw.rect(screen, plyr_clr, (x, y, 20, 20)) # Draw red square
+    pygame.draw.rect(screen, plyr_clr, (xpos, ypos, hght, wdth)) # Draw red square
     pygame.display.update()
-    # Control frame rate
+
+    #fps to stop crashes
     clock.tick(60)
-    #TEST
-    print("a")
